c1d58b57d9a9adf0c5c96aacfed64741726305d1
Patrick Dowell <patrickdowell@patrick-ds-macbook-air.local>
2019-02-26 11:37:35 -0600

    Parse JSON data

--- API Calling/CharactersViewController.swift
+++ API Calling/CharactersViewController.swift
@@ -15,9 +15,36 @@ class CharactersViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let query = "https://rickandmortyapi.com/api/character/"
-        // Do any additional setup after loading the view, typically from a nib.
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["status"] == "ok" {
+                    parse(json: json)
+                    return
+                }
+            }
+        }
+        loadError() 
     }
 
+    func parse(json: JSON) {
+        for result in json["results"].arrayValue {
+            let id = result["id"].stringValue
+            let name = result["name"].stringValue
+            let status = result["status"].stringValue
+            let species = result["species"].stringValue
+            let gender = result["gender"].stringValue
+            let character = ["id": id, "name": name, "status": status, "species": species, "gender": gender]
+            characters.append(character)
+        }
+        tableView.reloadData()
+    }
 
+    func loadError() {
+        let alert = UIAlertController(title: "Loading Error", message: "There was a problem loading the news feed.", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
